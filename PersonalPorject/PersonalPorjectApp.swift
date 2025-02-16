7ec7ad283dbd2690bfd3a829f34689005e1becd9
pranay <pbhaturkar5@gmail.com>
2025-02-16 16:22:19 +0530

    Initial Commit

---
+++ PersonalPorject/PersonalPorjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalPorjectApp.swift
+//  PersonalPorject
+//
+//  Created by Pranay2 on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalPorjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
